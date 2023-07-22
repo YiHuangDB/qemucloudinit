3ef741d3f379aef57b7af88094b2b96bad461164
Yi Huang <yi.huang@ixsystems.com>
2023-07-22 20:09:34 +0000

    Sample script to build and test cloudinit qemu

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ qemu-system-x86_64 \
 -enable-kvm -cpu host \
 -m 4096 -overcommit mem-lock=off -smp 6,sockets=6,cores=1,threads=1 \
 -drive file=disk.qcow2,index=0,cache=none,aio=threads,if=virtio \
--drive file=seed.iso,index=1,cache=none,aio=threads,if=virtio \
 -net nic,model=virtio,macaddr=$(cat /sys/class/net/macvtap10/address) \
--net tap,fd=4 4<>/dev/tap$(cat /sys/class/net/macvtap10/ifindex) 
-#-boot d \
+-net tap,fd=4 4<>/dev/tap$(cat /sys/class/net/macvtap10/ifindex) \
+-drive driver=raw,file=seed.iso,if=virtio \
+#-boot order=n,splash=/home/ubuntu/qemucloudinit/truenasr.jpeg,splash-time=5000,strict=on \
+#-bios /usr/share/OVMF/OVMF_CODE.fd \
